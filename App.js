2243f8f41602665340d38400f015aba858672f08
Ian Walls <wallsianh@gmail.com>
2021-02-01 14:56:29 -0500

    [removed] app js basic text

--- App.js
+++ App.js
@@ -1,32 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import HomePage from "./HomePage";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './HomePage';
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import MockChallenges from "./MockChallenges";
+import { createStackNavigator } from '@react-navigation/stack'
+import MockChallenges from './MockChallenges';
 
 const HomeStack = createStackNavigator();
 
-export default class App {
-  render() {
-    return (
-      <NavigationContainer>
-        <HomeStack.Navigator>
-          <HomeStack.Screen name="HomePage" component={HomePage} />
-          <HomeStack.Screen name="MockChallenges" component={MockChallenges} />
-        </HomeStack.Navigator>
-      </NavigationContainer>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <HomeStack.Navigator>
+        <HomeStack.Screen name="HomePage" component={HomePage} />
+        <HomeStack.Screen name="MockChallenges" component={MockChallenges} />
+      </HomeStack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
