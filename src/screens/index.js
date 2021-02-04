3331d4208a0e6a34d3c8c7ea7a0e554cec084801
Morganne Wray <morgannewray@gmail.com>
2021-02-04 15:17:53 -0500

    index file

---
+++ src/screens/index.js
@@ -0,0 +1,14 @@
+export { default as LoginScreen } from "./LoginScreen";
+export { default as RegistrationScreen } from "./RegistrationScreen";
+export { default as HomePage } from "./HomePage";
+export { default as AuthScreen } from "./AuthScreen";
+export { default as PersonalChallengesScreen } from "./PersonalChallengesScreen";
+export { default as FriendChallengesScreen } from "./FriendChallengesScreen";
+export { default as ChallengeTrackerScreen } from "./ChallengeTrackerScreen";
+export { default as ChallengeDetailsScreen } from "./ChallengeDetailsScreen";
+//export { default as LoadingScreen } from "./LoadingScreen";
+export { default as Friends } from "./Friends";
+export { default as ProfileScreen } from "./ProfileScreen";
+export { default as HomeIcon } from "./Icons/HomeIcon";
+export { default as FriendsIcon } from "./Icons/FriendsIcon";
+export { default as UserIcon } from "./Icons/UserIcon";
