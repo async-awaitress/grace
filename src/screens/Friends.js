79a685ec28323b7b4b870f7d88fc3044732884fc
Morganne Wray <morgannewray@gmail.com>
2021-02-08 15:01:08 -0500

    friends page update

--- src/screens/Friends.js
+++ src/screens/Friends.js
@@ -4,20 +4,27 @@ import { View, Text, StyleSheet } from "react-native";
 const Friends = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Friends!!!</Text>
+      <Text style={styles.title}>
+        Please pardon this page while it's under construction.
+      </Text>
+      <Text style={styles.title}>🚧🚧🚧🚧🚧</Text>
+      <Text style={styles.title}>Friends are coming soon!</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 18,
+    fontSize: 32,
     fontWeight: "bold",
     marginLeft: 15,
-    marginBottom: 5,
+    marginTop: 80,
+    textAlign: "center",
+    color: "white",
   },
   container: {
     flex: 1,
+    backgroundColor: "orange",
   },
 });
 
